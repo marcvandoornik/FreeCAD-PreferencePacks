d87b8bd3c6dade4ac0c1972ed7ee1dc8d42d1fe2
Marc van Doornik | VB-Airsuspension NL <marc.vandoornik@vbairsuspension.com>
2026-03-23 16:19:54 +0100

    Added macro path to the launcher

--- freecad.bat
+++ freecad.bat
@@ -5,7 +5,7 @@ REM SETTINGS FOR THIS FREECAD INSTANCE
 REM ============================================================================
 
 REM The name of the FreeCAD installation directory under %BIN_DIR%
-set FREECAD_DIR=freecad
+set FREECAD_DIR=freecad_1.1RC3
 
 REM Name for the program title bar
 set EXE_NAME="FreeCAD Portable"
@@ -22,6 +22,7 @@ set QT_QPA_PLATFORM=windows:darkmode=2
 
 set FREECAD_USER_HOME=%~dp0data
 set FREECAD_USER_TEMP=%~dp0temp
+set FREECAD_MACRO_DIR=%~dp0macros
 
 
 REM ============================================================================
@@ -36,11 +37,12 @@ REM LAUNCH FREECAD
 REM ============================================================================
 
 start %BIN_DIR%\%FREECAD_DIR%\bin\freecad.exe^
+ launcher\freecad_startup.py^
  %1 %2 %3 %4 %5 %6 %7 %8 %9^
  -l^
  --set-config ExeName=%EXE_NAME%^
  --set-config ExeVersion=%EXE_VERSION%^
  --set-config AppIcon=%BRANDING_DIR%\freecad.ico^
  --set-config SplashScreen=%BRANDING_DIR%\SplashScreen.png^
- --set-config ProgramLogo=%BRANDING_DIR%\freecad.ico^
- --set-config ProgramIcons=%BRANDING_DIR%\freecad.ico
+ --set-config ProgramIcons=%BRANDING_DIR%\freecad.ico^
+ --set-config ProgramLogo=%BRANDING_DIR%\freecad.ico
